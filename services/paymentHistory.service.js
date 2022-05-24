4439681c8e9e6080e28eb8edd2e52cbd97026698
sindetisarah <sindetisarah@gmail.com>
2022-05-24 21:40:35 +0300

    create payment service

---
+++ services/paymentHistory.service.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const request = require('request');
+exports.getPaymentHistory= async (value) => {
+    return new Promise(async (resolve, reject) => {
+        var options = {
+            'method': 'GET',
+            'url': `https://api.sunculture.io/stage/paymentByAccountRef/${value}`,
+        };
+        await request(options, async (error, response) => {
+            if (error) {
+                console.error(error)
+                reject(`Request failed. Cannot fetch payment history: ${error}`)
+            } else {
+                let responseData= JSON.parse(response.data)
+                console.info(`[Data from api.sunculture]: ${JSON.stringify(responseData)}`)
+                if (responseData.length == 0) {
+                    reject("You have no payment made yet .")
+                } else {
+                    resolve(responseData)
+                }
+            }
+        });
+    })
+}
