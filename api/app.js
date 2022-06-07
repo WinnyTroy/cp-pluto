e818ba8a26e04bc2c59eccdb34eb7acb25ecf597
Evans Kiptarus Kibet <evanskibet114@gmail.com>
2022-06-07 11:36:56 +0300

    Added the sentry implementation

--- api/app.js
+++ api/app.js
@@ -5,7 +5,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 const helpers = require('./helpers/index')
+const Sentry = require("@sentry/node")
+const Tracing = require("@sentry/tracing")
 var app = express();
+
+Sentry.init({
+  dsn: "https://9f9dd90308714321bd222a65679fe52c@o1162014.ingest.sentry.io/6474657",
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,12 +30,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use('/', require('./routes/index'));
 app.use('/api/v1', require('./routes/users'));
 app.use('/api/v1', require('./routes/tickets'))
 app.use('/api/v1', require('./routes/devices'))
 app.use('/api/v1', require('./routes/payments'))
 app.use('/api/v1', require('./routes/auth'))
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(function (req, res, next) {
   return helpers.response.notFoundResponse(res, "Api resource not found")
