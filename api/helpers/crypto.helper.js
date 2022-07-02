bf750a4e871f858efca5e39f10cf4c1c84a047f6
Evans Kiptarus Kibet <evanskibet114@gmail.com>
2022-07-02 08:13:59 +0300

    fix the crypto encryption

--- api/helpers/crypto.helper.js
+++ api/helpers/crypto.helper.js
@@ -8,10 +8,9 @@ exports.encrypt = (jsonData, cipherKey) => {
 exports.decrypt = (ciphertext, cipherKey) => {
     let bytes = CryptoJS.AES.decrypt(ciphertext, cipherKey);
 
-    console.info(bytes.sigBytes)
+    console.info(JSON.stringify(bytes))
 
-    let decryptedData = bytes.sigBytes > 0 ? JSON.parse(bytes.toString(CryptoJS.enc.Utf8)) : "Invalid encryption key"
-
+    let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
 
     return decryptedData
 }
--- api/services/crypto.service.js
+++ api/services/crypto.service.js
@@ -5,20 +5,29 @@ exports.encrypt = async (req, res, next) => {
 
         let body = Object.keys(req.body).length > 0 ? req.body : {}
 
+        let params = Object.keys(req.params).length > 0 ? req.params : {}
+
+        let query = Object.keys(req.query).length > 0 ? req.query : {}
+
         let encryptedBody = await helpers.crypto.encrypt(body, req.body.cipherKey)
 
-        resolve(encryptedBody)
+        let encryptedParams = await helpers.crypto.encrypt(params, req.body.cipherKey)
+
+        let encryptedQuery = await helpers.crypto.encrypt(query, req.body.cipherKey)
+
+        resolve({ body: encryptedBody, params: encryptedParams, query: encryptedQuery })
     })
 }
 
 exports.decrypt = async (req, res, next) => {
     return new Promise(async (resolve, reject) => {
-        //handle the req.body, req.params, req.query
 
-        let body = Object.keys(req.body).length > 0 ? req.body : {}
+        let decryptedBody = await helpers.crypto.decrypt(req.body.body, req.body.cipherKey)
 
-        let decryptedBody = await helpers.crypto.decrypt(req.body.data, req.body.cipherKey)
+        let decryptedParams = await helpers.crypto.decrypt(req.body.params, req.body.cipherKey)
 
-        resolve(decryptedBody)
+        let decryptedQuery = await helpers.crypto.decrypt(req.body.query, req.body.cipherKey)
+
+        resolve({ body: decryptedBody, params: decryptedParams, query: decryptedQuery })
     })
 }
